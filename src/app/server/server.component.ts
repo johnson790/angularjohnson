4454d5175625bf9aad9db3cf4f7e4d0ec1f2e4d5
johnson790 <johnsoncephakyando@gmail.com>
2023-08-19 14:38:11 +0300

    Creating a feature for background color where online status is green and offline status is red.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,4 +11,10 @@ serverStatus: string ="offline";
 getServerStatus() {
     return this.serverStatus;
 }
+constructor(){
+    this.serverStatus = Math.random()> 0.5? 'online' : 'offline';
+}
+getColor(){
+    return this.serverStatus === 'online'?'green':'red'
+}
 }
